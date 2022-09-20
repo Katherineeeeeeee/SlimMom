531cf5f86ae64ce230c701cf0ee4c944aef99f52
Denis Dashkevich <majorprestige99@gmail.com>
2022-09-20 20:19:18 +0300

    update helpers

--- src/components/Shared/Logo/Logo.jsx
+++ src/components/Shared/Logo/Logo.jsx
@@ -1,37 +1,43 @@
 import { Link } from 'react-router-dom';
-import { isRetina, isDesctop, isTablet, isMobile } from 'helpers/screen-sizes';
+// import { isRetina, isDesctop, isTablet, isMobile } from 'helpers/screen-sizes';
+import { useMediaQuery } from 'react-responsive';
 
 const Logo = () => {
+  const isDesctop = useMediaQuery({ query: '(max-width: 1280px)' });
+  const isTablet = useMediaQuery({ query: '(max-width: 768px)' });
+  const isMobile = useMediaQuery({ query: '(max-width: 480px)' });
+  const isRetina = useMediaQuery({ query: '(min-resolution: 2dppx)' });
+
   return (
     <>
       {isMobile && (
-        <Link>
-          <img src="" alt="" />
+        <Link to="/dairy">
+          <img src="../../../images/logo/logo-mob.png" alt="web-site logo" />
         </Link>
       )}
       {isTablet && (
-        <Link>
-          <img src="" alt="" />
+        <Link to="/dairy">
+          <img src="" alt="web-site logo" />
         </Link>
       )}
       {isDesctop && (
-        <Link>
-          <img src="" alt="" />
+        <Link to="/dairy">
+          <img src="" alt="web-site logo" />
         </Link>
       )}
       {isMobile && isRetina && (
-        <Link>
-          <img src="" alt="" />
+        <Link to="/dairy">
+          <img src="" alt="web-site logo" />
         </Link>
       )}
       {isTablet && isRetina && (
-        <Link>
-          <img src="" alt="" />
+        <Link to="/dairy">
+          <img src="" alt="web-site logo" />
         </Link>
       )}
       {isDesctop && isRetina && (
-        <Link>
-          <img src="" alt="" />
+        <Link to="/dairy">
+          <img src="" alt="web-site logo" />
         </Link>
       )}
     </>
