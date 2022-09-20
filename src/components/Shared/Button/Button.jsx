5d6d6d07507705a5094b0ec854ab0e9fce2c7224
Andrii Klochkov <lord@MacBook-Pro-Andrii.local>
2022-09-20 16:20:01 +0300

    style/addProps/button

--- src/components/Shared/Button/Button.jsx
+++ src/components/Shared/Button/Button.jsx
@@ -1,5 +1,7 @@
-const Button = () => {
-  return <div></div>;
+import s from './Button.module.scss';
+
+const Button = ({text='+', type='submit', btnClass='btnPlus', handleClick}) => {
+  return <button className={s[btnClass]} onClick={handleClick} type={type}>{text}</button>;
 };
 
 export default Button;
