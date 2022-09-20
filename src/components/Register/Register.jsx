7947a38bde7edbaecab1e2bfc18d5d66c0c1a618
Katherineeeeeeee <katherine.dudar.7@gmail.com>
2022-09-20 21:00:37 +0300

    completed markup register form

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,5 +1,24 @@
+import s from './Register.module.scss';
+import Container from 'components/Shared/Container';
+import TextField from 'components/Shared/TextField';
+import { field } from 'components/Shared/TextField/fields';
+import Button from 'components/Shared/Button';
+
 const Register = () => {
-  return <div></div>;
+  return (
+    <Container>
+      <h2 className={s.title}>Register</h2>
+      <form className={s.form}>
+        <TextField {...field.name} />
+        <TextField {...field.email} />
+        <TextField {...field.password} />
+      </form>
+      <div className={s.wrap}>
+        <Button text="Log in" btnClass="btn" />
+        <Button text="Register" btnClass="btnLight" />
+      </div>
+    </Container>
+  );
 };
 
 export default Register;
