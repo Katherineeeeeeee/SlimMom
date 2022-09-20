06a05c25da364ef844b60db3a193fbdb95d4ff48
Katherineeeeeeee <katherine.dudar.7@gmail.com>
2022-09-20 16:42:30 +0300

    worked on TextField

---
+++ src/components/Shared/TextField/fields.jsx
@@ -0,0 +1,21 @@
+export const field = {
+  name: {
+    name: 'name',
+    type: 'text',
+    placeholder: 'Name',
+    required: true,
+  },
+
+  email: {
+    name: 'email',
+    type: 'email',
+    placeholder: 'Email',
+    required: true,
+  },
+  password: {
+    name: 'password',
+    type: 'password',
+    placeholder: 'Password',
+    required: true,
+  },
+};
