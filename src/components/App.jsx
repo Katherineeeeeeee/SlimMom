20eed61ce22b3d5b65f10aa6e2ff186d41ebfe28
Denis Dashkevich <majorprestige99@gmail.com>
2022-09-20 17:34:36 +0300

    added Header

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,11 @@
 import Container from './Shared/Container';
 import UserRoutes from './Routes/UserRoutes';
+import Header from './Header';
 
 export const App = () => {
   return (
     <Container>
+      <Header />
       <UserRoutes />
     </Container>
   );
