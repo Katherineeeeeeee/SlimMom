9e3db75657586cee1ce25727fe231b3520967d37
Sergey Gulaga <ry6ens@gmail.com>
2022-09-21 13:02:04 +0300

    update

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -4,14 +4,29 @@ import TextField from 'components/Shared/TextField';
 import { field } from 'components/Shared/TextField/fields';
 import Button from 'components/Shared/Button';
 
-const Register = () => {
+import useForm from '../../helpers/hooks/useForm';
+import initialState from './initialState';
+
+const Register = ({ onSubmit }) => {
+  const { state, handleChange, handleSubmit } = useForm({
+    initialState,
+    onSubmit,
+  });
+
+  const { name, email, password } = state;
+  console.log(state);
+
   return (
     <Container>
       <h2 className={s.title}>Register</h2>
-      <form className={s.form}>
-        <TextField {...field.name} />
-        <TextField {...field.email} />
-        <TextField {...field.password} />
+      <form className={s.form} onSubmit={handleSubmit}>
+        <TextField {...field.name} value={name} handleChange={handleChange} />
+        <TextField {...field.email} value={email} handleChange={handleChange} />
+        <TextField
+          {...field.password}
+          value={password}
+          handleChange={handleChange}
+        />
       </form>
       <div className={s.wrap}>
         <Button text="Log in" btnClass="btn" />
@@ -22,3 +37,7 @@ const Register = () => {
 };
 
 export default Register;
+
+Register.defaultProps = {
+  onSubmit: () => {},
+};
