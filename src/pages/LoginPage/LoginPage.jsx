208afebde04e27172f26c798d05eb3ae25670f0f
Katherineeeeeeee <katherine.dudar.7@gmail.com>
2022-09-21 15:24:48 +0300

    added bcg to Login/Register page

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,5 +1,16 @@
+import s from '../../components/Login/Login.module.scss';
+import Login from 'components/Login';
+import bcgDesktop from '../../images/desktop/bcgD.png';
+import bcgTablet from '../../images/tablet/bcg.png';
+
 const LoginPage = () => {
-  return <div></div>;
+  return (
+    <>
+      <Login />
+      <img className={s.imgTablet} src={bcgTablet} alt="background" />
+      <img className={s.imgDesktop} src={bcgDesktop} alt="background" />
+    </>
+  );
 };
 
 export default LoginPage;
--- src/pages/RegisterPage/RegisterPage.jsx
+++ src/pages/RegisterPage/RegisterPage.jsx
@@ -1,5 +1,16 @@
+import s from '../../components/Register/Register.module.scss';
+import Register from 'components/Register';
+import bcgDesktop from '../../images/desktop/bcgD.png';
+import bcgTablet from '../../images/tablet/bcg.png';
+
 const RegisterPage = () => {
-  return <div></div>;
+  return (
+    <>
+      <Register />
+      <img className={s.imgTablet} src={bcgTablet} alt="background" />
+      <img className={s.imgDesktop} src={bcgDesktop} alt="background" />
+    </>
+  );
 };
 
 export default RegisterPage;
