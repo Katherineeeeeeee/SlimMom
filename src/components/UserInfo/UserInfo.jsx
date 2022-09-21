d67f0afa0848151e456f6b3e59750f9dd74aee8d
Andrii Klochkov <lord@MacBook-Pro-Andrii.local>
2022-09-21 19:23:25 +0300

    useStore/fixed Slice

--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,10 +1,17 @@
 import { NavLink } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
 
-// import s from './UserInfo.module.scss';
-
-const isUserLogin = 0;
+import { getLogin } from 'redux/auth/auth-selectors';
+import { logout } from 'redux/auth/auth-opetations';
 
 const UserInfo = () => {
+const isUserLogin = useSelector(getLogin);
+const dispatch = useDispatch();
+
+const onLogout = () => {
+  dispatch(logout());
+}
+
   if (!isUserLogin) {
     return (
       <div className="wrap">
@@ -18,7 +25,7 @@ const UserInfo = () => {
     return (
       <div className="wrap">
         <p>Nic</p>
-        <p>Exit</p>
+        <p onClick={onLogout}>Exit</p>
       </div>
     );
   }
