89077b24ef6b417c59527295460e3db2dab03247
Andrii Klochkov <lord@MacBook-Pro-Andrii.local>
2022-09-22 00:57:25 +0300

    fixed header/fields/nav

--- src/components/Shared/TextField/fields.jsx
+++ src/components/Shared/TextField/fields.jsx
@@ -14,7 +14,7 @@ export const field = {
     type: 'email',
     placeholder: 'Email',
     required: true,
-    pattern: '/^[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}$/',
+    // pattern: '/^[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}$/',
     // pattern:"[^@\s]+@[^@\s]+\.[^@\s]+",
     title: '',
   },
--- src/redux/auth/auth-selectors.js
+++ src/redux/auth/auth-selectors.js
@@ -1,2 +1,3 @@
 export const getLogin = ({ auth }) => auth.isLogin;
+export const getUserName = ({ auth }) => auth.user.username;
 export const getSid = ({ auth }) => auth.sid;
