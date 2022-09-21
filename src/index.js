8e627474f2c3d29647a0e58037f6f07760693a2f
Denis Dashkevich <majorprestige99@gmail.com>
2022-09-21 12:34:03 +0300

    create Redux store

--- src/index.js
+++ src/index.js
@@ -2,12 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from 'redux/store';
+
 import './styles/styles.scss';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>
 );
---
+++ src/redux/auth/auth-slice.js
@@ -0,0 +1,90 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { register, login, logout, refresh } from './auth-opetations';
+
+const initialState = {
+  user: {},
+  todaySummary: {},
+  sid: '',
+  accessToken: '',
+  refreshToken: '',
+  isLogin: false,
+  loading: false,
+  error: null,
+};
+
+const accessAuth = (store, payload) => {
+  store.loading = false;
+  store.isLogin = true;
+  store.user = payload.user;
+  store.sid = payload.sid;
+  store.accessToken = payload.accessToken;
+  store.refreshToken = payload.refreshToken;
+};
+
+const auth = createSlice({
+  name: 'auth',
+  initialState,
+  extraReducers: {
+    // * REGISTER
+
+    [register.pending]: store => {
+      store.loading = true;
+      store.error = null;
+    },
+
+    [register.fulfilled]: (store, { payload }) => accessAuth(store, payload),
+
+    [register.rejected]: (store, { payload }) => {
+      store.loading = false;
+      store.error = payload;
+    },
+
+    // * LOGIN
+
+    [login.pending]: store => {
+      store.loading = true;
+      store.error = null;
+    },
+
+    [login.fulfilled]: (store, { payload }) => accessAuth(store, payload),
+
+    [login.rejected]: (store, { payload }) => {
+      store.loading = false;
+      store.error = payload;
+    },
+
+    // * LOGOUT
+
+    [logout.pending]: store => {
+      store.loading = true;
+      store.error = null;
+    },
+    [logout.fulfilled]: () => ({ ...initialState }),
+    [logout.rejected]: (store, { payload }) => {
+      store.loading = false;
+      store.error = payload;
+    },
+
+    // * REFRESH
+
+    [refresh.pending]: store => {
+      store.loading = true;
+      store.error = null;
+    },
+
+    [refresh.fulfilled]: (store, { payload }) => {
+      store.isLogin = true;
+      store.loading = false;
+      store.sid = payload.sid;
+      store.accessToken = payload.accessToken;
+      store.refreshToken = payload.refreshToken;
+    },
+
+    [refresh.rejected]: (store, { payload }) => {
+      store.loading = false;
+      store.error = payload;
+    },
+  },
+});
+
+export default auth.reducer;
