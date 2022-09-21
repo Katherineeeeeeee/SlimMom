daf27d6393d1fe9647a816344d908253f89c55b1
Denis Dashkevich <majorprestige99@gmail.com>
2022-09-21 20:46:22 +0300

    Update HomePage.jsx

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,9 +1,8 @@
 import Home from 'components/Home';
-// import Modal from '../../components/Modal/Modal';
-import { dailyRateInfo } from "../../redux/daily-rate/daily-rate-operations"
-import  dailyRateSelectors  from "../../redux/daily-rate/daily-rate-selectors"
+import Modal from '../../components/Modal/Modal';
+import { dailyRateInfo } from '../../redux/daily-rate/daily-rate-operations';
+import dailyRateSelectors from '../../redux/daily-rate/daily-rate-selectors';
 import { useDispatch, useSelector } from 'react-redux';
-
 
 const HomePage = () => {
   const dispatch = useDispatch();
@@ -11,13 +10,13 @@ const HomePage = () => {
 
   const onGetDailyRate = store => {
     const action = dailyRateInfo(store);
-    dispatch(action)
+    dispatch(action);
   };
 
   return (
     <div>
       <Home onSubmit={onGetDailyRate} />
-      {/* <Modal /> */}
+      <Modal />
     </div>
   );
 };
