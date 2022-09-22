f284ae66fa5d87247621e4e0b4aac226ef5001f2
Katherineeeeeeee <katherine.dudar.7@gmail.com>
2022-09-22 18:26:50 +0300

    added bcg to HomePage, added Loader in index.js

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,7 +1,17 @@
+import s from '../../components/Home/Home.module.scss';
+import bcgDesktop from '../../images/desktop/bcgD.png';
+import bcgTablet from '../../images/tablet/bcgCalcTablet.png';
+
 import Home from 'components/Home';
 
 const HomePage = () => {
-  return <Home />;
+  return (
+    <>
+      <Home />
+      <img className={s.imgTablet} src={bcgTablet} alt="background" />
+      <img className={s.imgDesktop} src={bcgDesktop} alt="background" />
+    </>
+  );
 };
 
 export default HomePage;
