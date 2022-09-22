c702a534dc3f3aa0f9141c0df9957109a93789f3
Andrii Klochkov <lord@MacBook-Pro-Andrii.local>
2022-09-22 14:30:11 +0300

    fixed(modal/headerLinks)

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,9 +1,8 @@
-import Button from 'components/Shared/Button';
 import { useEffect } from 'react';
 
 import s from './Modal.module.scss';
 
-const Modal = ({ setModalOpen }) => {
+const Modal = ({ setModalOpen, children }) => {
   useEffect(() => {
     const onEscPush = e => {
       if (e.code !== 'Escape') {
@@ -36,20 +35,7 @@ const Modal = ({ setModalOpen }) => {
           <span onClick={closeModal} className={s.closeM}></span>
         </div>
         <span onClick={closeModal} className={s.closeOther}></span>
-        <h2 className={s.modalTitle}>
-          Your recommended daily calorie intake is
-        </h2>
-        <p className={s.modalText}>
-          2800<span className={s.textDescription}> kcal</span>
-        </p>
-        <div className={s.menuGroup}>
-          <p className={s.menuGroupTitle}>Foods you should not eat</p>
-          <ol className={s.menuGroupList}>
-            <li className={s.menuGroupItems}>Flour products</li>
-            <li className={s.menuGroupItems}>Milk</li>
-          </ol>
-        </div>
-        <Button text="Start losing weight" type="submit" btnClass="btn" />
+        {children}
       </div>
     </div>
   );
