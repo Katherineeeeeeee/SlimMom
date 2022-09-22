81ede0a2f7fa6c848bee022786c1e8dfd83ae37b
Maksym150678 <civicm110@gmail.com>
2022-09-22 19:00:33 +0300

    create sidebar

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,5 +1,35 @@
+import styles from '../SideBar/SideBar.module.scss';
+
 const SideBar = () => {
-  return <div></div>;
+  return (
+  <div className={styles.container_sidebar}> 
+  <div className={styles.sum}>
+    <h3 className={styles.title_sidebar}>Summary for 06/20/2020</h3>
+  <ul className={styles.list_sidebar}>
+    <li className={styles.item_sidebar}>
+      <p className={styles.text_sidebar_sum}>Left</p> 
+      <span className={styles.data}>000 kcal</span>
+    </li>
+    <li className={styles.item_sidebar}>
+      <p className={styles.text_sidebar}>Consumed</p> 
+      <span className={styles.data}>000 kcal</span>
+    </li>
+    <li className={styles.item_sidebar}>
+      <p className={styles.text_sidebar}>Daily rate</p> 
+      <span className={styles.data}>000 kcal</span>
+    </li>
+    <li className={styles.item_sidebar}>
+      <p className={styles.text_sidebar}>n% of normal</p> 
+      <span className={styles.data}>000 kcal</span>
+    </li>
+  </ul>
+  </div>
+  <div className={styles.food}>
+  <h3 className={styles.title_sidebar}>Food not recommended</h3>
+  <p className={styles.text_sidebar_food}>Your diet will be displayed here</p>  
+  </div>
+  </div>
+  )
 };
 
 export default SideBar;
