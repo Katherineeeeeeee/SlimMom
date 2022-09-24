341ac9a97d68a613f3c8285df7ba02488db54633
Denis Dashkevich <majorprestige99@gmail.com>
2022-09-24 20:17:04 +0300

    update conflict

--- src/pages/DairyPage/DairyPage.jsx
+++ src/pages/DairyPage/DairyPage.jsx
@@ -1,10 +1,10 @@
 import { useMediaQuery } from 'react-responsive';
 import { Link } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
-import { getID } from 'redux/auth/auth-selectors';
-import { useEffect } from 'react';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { getID } from 'redux/auth/auth-selectors';
+// import { useEffect } from 'react';
 
-import Button from 'components/Shared/Button';
+// import Button from 'components/Shared/Button';
 import SideBar from 'components/SideBar';
 
 import DiaryAddProductForm from '../../components/Dairy/DiaryAddProductForm';
@@ -13,13 +13,10 @@ import DiaryProductsList from '../../components/Dairy/DiaryProductsList';
 
 import s from './DairyPage.module.scss';
 
-
-import { dailyRateUser } from 'redux/daily-rate/daily-rate-operations';
-import { getUserParams } from 'redux/userParams/user-params-selectors';
+// import { dailyRateUser } from 'redux/daily-rate/daily-rate-operations';
 
 // import { useEffect } from 'react';
 // import { useDispatch } from 'react-redux';
-
 
 const DairyPage = () => {
   const isMobile = useMediaQuery({ maxWidth: 767 });
@@ -27,9 +24,7 @@ const DairyPage = () => {
 
   const isTabletDesktop = useMediaQuery({ minWidth: 767 });
 
-
   // useEffect(() => {}, []);
-
 
   return (
     <main className={s.wrapper}>
